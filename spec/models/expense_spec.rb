6f57795726165c69cab960d077dfcf03cbfa6d26
Joseph Lee <jhjlee702@gmail.com>
2024-01-31 16:14:34 -0700

    create income attributes tests

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Expense, type: :model do
   it "creates an expense with all attributes" do
     user = User.create(username: "josephlee", email: "josephlee@gmail.com")
     expense = Expense.create(user_id: user.id, category: "food", amount: 23.75, date: "2023-05-13")
+    
     expect(expense).to have_attributes(user_id: user.id)
     expect(expense).to have_attributes(category: "food")
     expect(expense).to have_attributes(amount: 23.75)
--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Income, type: :model do
   describe "relationships" do
     it {should belong_to :user}
   end
+
+  it "creates an income with all attributes" do
+    user = User.create(username: "josephlee", email: "josephlee@gmail.com")
+    income = Income.create(user_id: user.id, source: "paycheck", amount: 2375.29, date: "2023-01-31")
+    
+    expect(income).to have_attributes(user_id: user.id)
+    expect(income).to have_attributes(source: "paycheck")
+    expect(income).to have_attributes(amount: 2375.29)
+    expect(income.date.strftime("%Y-%m-%d")).to eq("2023-01-31")
+  end
 end
