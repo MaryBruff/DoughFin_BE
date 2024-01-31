7c23f0c257fa7cc22ba275e37b23540038b294bd
Joseph Lee <jhjlee702@gmail.com>
2024-01-31 15:34:15 -0700

    write relationship tests for expense, income and user models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it {should have_many :incomes}
+    it {should have_many :expenses}
+  end
 end
