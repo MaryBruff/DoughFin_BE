23e0ac6d7eeae63c2a343c1e495907977674f559
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-03 11:44:59 -0700

    Add sad path testing for delete expense expense does not exist

--- spec/graphql/mutations/expenses/delete_expense_request_spec.rb
+++ spec/graphql/mutations/expenses/delete_expense_request_spec.rb
@@ -31,4 +31,34 @@ RSpec.describe Mutations::DeleteExpense, type: :request do
       expect(Expense.all.length).to eq(4)
     end
   end
+
+  describe "sad paths" do
+    it "will gracefully handle no expense found" do
+      user = create(:user)
+      user.expenses = create_list(:expense, 3)
+
+      mutation = <<~GQL
+        mutation {
+          deleteExpense(input: { expenseId: 123123123}) {
+              code
+              message
+              success
+              }
+            }
+      GQL
+
+      expect(user.expenses.length).to eq(3)
+
+      post '/graphql', params: { query: mutation }
+
+      json_response = JSON.parse(response.body)
+      data = json_response['data']['deleteExpense']
+
+      expect(data['code']).to eq(404)
+      expect(data['message']).to eq("No expense found with expenseId 123123123")
+      expect(data['success']).to be(false)
+
+      expect(Expense.all.length).to eq(3)
+    end
+  end
 end
