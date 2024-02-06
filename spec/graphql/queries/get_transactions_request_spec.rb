1657d4755a1e1332dc77d95b9611b0376d81c2dd
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-05 17:41:23 -0700

    Write test for successfully get transactions for a user

--- spec/graphql/queries/get_transactions_request_spec.rb
+++ spec/graphql/queries/get_transactions_request_spec.rb
@@ -1,44 +1,48 @@
 require "rails_helper"
 
 RSpec.describe "Get Transactions", type: :request do
-  let!(:user) { create :user }
-
-  let(:query) do
-    <<-GRAPHQL
-      query getTransactions($userId: ID!) {
-        user(id: $userId) {
-          user {
-            id
-          }
-          transactions
-        }
-      }
-    GRAPHQL
-  end
-
   it "returns all of a user's transactions" do
     user = create(:user)
     user.expenses = create_list(:expense, 5)
     user.incomes = create_list(:income, 5)
+   
+    query =  <<~GQL
+        query getTransactions($email: String!) {
+          user(email: $email) {
+            id
+            transactions {
+              id
+              amount
+              date
+              source
+              type
+            }
+          }
+        }
+      GQL
 
-    post "/graphql", params: {query: query, variables: {userId: user.id}}
+    post "/graphql", params: {query: query, variables: {email: user.email}}
 
     json = JSON.parse(response.body, symbolize_names: true)
     data = json[:data]
 
     expect(data[:user][:id]).to eq(user.id.to_s)
-    data[:expenses].each do |transaction|
+
+    data[:user][:transactions].each do |transaction|
       expect(transaction).to have_key(:id)
-      expect(transaction[:id]).to be_a Integer
+      expect(transaction[:id].to_i).to be_a Integer
       
-      expect(transaction).to have_key(:description)
-      expect(transaction[:description]).to be_a Integer
+      expect(transaction).to have_key(:source)
+      expect(transaction[:source]).to be_a String
 
       expect(transaction).to have_key(:amount)
-      expect(transaction[:amount]).to be_a Integer
+      expect(transaction[:amount]).to be_a Float
 
       expect(transaction).to have_key(:date)
-      expect(transaction[:date]).to be_a Integer
+      expect(transaction[:date]).to be_a String
+
+      expect(transaction).to have_key(:type)
+      expect(transaction[:type]).to be_a String
     end
   end
 end
