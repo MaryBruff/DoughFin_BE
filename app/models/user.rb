20afee54b9252430a97eed8cbdb62a4f796b3481
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-05 17:53:47 -0700

    Add user sql query for getting transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def transactions
+    transactions = User.find_by_sql("SELECT id, 
+                                           amount, 
+                                           source, 
+                                           date, 
+                                           'income' AS type FROM incomes WHERE user_id = #{id} 
+                                    UNION 
+                                    SELECT id, 
+                                           amount, 
+                                           category, 
+                                           date, 
+                                           'expense' AS type FROM expenses WHERE user_id = #{id} 
+                                    ORDER BY date")
+  end
 end
