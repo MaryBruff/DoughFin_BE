1a0e6bf7d6404ec1cc23a7f1ae1e9fe22f7bdf3a
Joseph Lee <jhjlee702@gmail.com>
2024-02-06 13:50:24 -0700

    GOT THE TOTAL INCOME QUERY

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,19 +4,23 @@ module Types
     field :transactions, [Types::TransactionType], null: true
     field :username, String, null: false
     field :email, String, null: false
-    field :incomes, [Types::IncomeType], null: true
-    field :total_income, Float, null: true
-    field :expenses, [Types::ExpenseType], null: true
-    field :total_expense, Float, null: true
+    field :currentIncome, Types::CurrentIncomeType, null: true do
+      description "Returns the current income and percent change"
+    end
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    def total_income
-      object.incomes.sum(:amount)
-    end
+    def currentIncome
+      current_month_income = object.incomes.where("date >= ? AND date <= ?", Date.today.at_beginning_of_month, Date.today.at_end_of_month).sum(:amount)
+      
+      previous_month_income = object.incomes.where("date >= ? AND date <= ?", 1.month.ago.at_beginning_of_month, 1.month.ago.at_end_of_month).sum(:amount)
 
-    def total_expense
-      object.expenses.sum(:amount)
+      pct_change =(previous_month_income.zero? ? 0 : (current_month_income - previous_month_income) / previous_month_income.to_f) * 100
+
+      {
+        amount: current_month_income,
+        pctChange: pct_change
+      }
     end
   end
 end
--- spec/graphql/queries/total_income_spec.rb
+++ spec/graphql/queries/total_income_spec.rb
@@ -4,18 +4,29 @@ RSpec.describe Queries::TotalIncomeQuery, type: :request do
   describe "resolve" do
     it "successfully gets the sum of all incomes for a user" do
       user = create(:user)
-      user.incomes = create_list(:income, 5)
+      user.incomes.create(source: "Current Month 1", amount: 100.0, date: "2024-02-06", created_at: "2024-02-06", updated_at: "2024-02-06")
+      user.incomes.create(source: "Current Month 2", amount: 100.0, date: "2024-02-06", created_at: "2024-02-06", updated_at: "2024-02-06")
+      user.incomes.create(source: "Current Month 3", amount: 100.0, date: "2024-02-06", created_at: "2024-02-06", updated_at: "2024-02-06")
+      user.incomes.create(source: "Current Month 4", amount: 100.0, date: "2024-02-06", created_at: "2024-02-06", updated_at: "2024-02-06")
+      user.incomes.create(source: "Current Month 5", amount: 100.0, date: "2024-02-06", created_at: "2024-02-06", updated_at: "2024-02-06")
+      user.incomes.create(source: "Previous Source", amount: 100.0, date: "2024-01-15", created_at: "2024-01-15", updated_at: "2024-01-15")
 
-      expect(user.incomes.length).to eq(5)
+      expect(user.incomes.length).to eq(6)
       
       query = <<~GQL
-        query {
-          totalIncome(userId: #{user.id})
+        query { 
+          user(email: "#{user.email}") {
+            currentIncome {
+              amount
+              pctChange
+            }
+          }
         }
       GQL
       
       post "/graphql", params: {query: query}
       json_response = JSON.parse(response.body, symbolize_names: true)
+      require 'pry'; binding.pry
       data = json_response[:data]
 
       expect(data).to have_key(:totalIncome)
