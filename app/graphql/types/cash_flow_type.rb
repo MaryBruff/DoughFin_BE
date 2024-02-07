e0f05a4307417405e0b17c4b7cbae2bc699c2c50
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-06 21:36:13 -0700

    Change fields on cashflow to year, month, total income and total expense

--- app/graphql/types/cash_flow_type.rb
+++ app/graphql/types/cash_flow_type.rb
@@ -2,7 +2,11 @@
 
 module Types
   class CashFlowType < Types::BaseObject
-    field :username, String, null: true
-    field :years, [Types::YearType], null: true
+    # field :username, String, null: true
+    # field :years, [Types::YearType], null: true
+    field :year, String, null: true
+    field :month, String, null: true
+    field :total_income, Float, null: true
+    field :total_expense, Float, null: true
   end
 end
