543280569cb6164d277729677bdb43ba608f8c9d
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-06 17:58:01 -0700

    Update test to include more return expectations

--- spec/graphql/queries/get_cash_flow_spec.rb
+++ spec/graphql/queries/get_cash_flow_spec.rb
@@ -28,6 +28,33 @@ RSpec.describe "Get Cash Flow", type: :request do
 
     json = JSON.parse(response.body, symbolize_names: true)
     data = json[:data]
-    require 'pry'; binding.pry
+    
+    expect(data).to have_key(:cashFlow)
+    expect(data[:cashFlow]).to be_a Hash
+
+    expect(data[:cashFlow]).to have_key(:username)
+    expect(data[:cashFlow][:username]).to be_a String
+    
+    expect(data[:cashFlow]).to have_key(:years)
+    expect(data[:cashFlow][:years]).to be_a Array
+    
+    data[:cashFlow][:years].each do |year|
+      expect(year).to have_key(:year)
+      expect(year[:year]).to be_a String
+
+      expect(year).to have_key(:months)
+      expect(year[:months]).to be_a Array
+
+      year[:months].each do |month|
+        expect(month).to have_key(:month)
+        expect(month[:month]).to be_a String
+
+        expect(month).to have_key(:income)
+        expect(month[:income]).to be_a Float
+
+        expect(month).to have_key(:expenses)
+        expect(month[:expenses]).to be_a Float
+      end
+    end
   end
 end
