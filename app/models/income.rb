d71c898c55ae56573e886c5256c61c29c54c8912
Taylor Pubins <tpubz@icloud.com>
2024-02-07 15:37:26 -0700

    Add validations to incomes/expenses

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,4 +2,6 @@ class Income < ApplicationRecord
   belongs_to :user
 
   validates :date, presence: true, date: true
+  validates :amount, presence: true, numericality: {greater_than: 0}
+  validates :source, presence: true
 end
--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 RSpec.describe Income, type: :model do
   describe "relationships" do
     it { should belong_to :user }
+    it { should validate_presence_of :source }
+    it { should validate_presence_of :amount }
+    it { should validate_presence_of :date }
   end
 
   it "creates an income with all attributes" do
