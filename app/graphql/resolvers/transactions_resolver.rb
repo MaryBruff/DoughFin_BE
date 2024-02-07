1a6fa1001f7293d068b27b992cca1b0fa06085dd
Anthea Yur <acyur6@gmail.com>
2024-02-07 01:05:48 -0600

    Adjusted to expenses

--- app/graphql/resolvers/transactions_resolver.rb
+++ app/graphql/resolvers/transactions_resolver.rb
@@ -1,17 +1,17 @@
 module Resolvers
-  class TransactionsResolver < Resolvers::BaseResolver
+  class ExpensesResolver < Resolvers::BaseResolver
     ## allows for optional additional queries such as month and category
     argument :month, String, required: false
     argument :category, String, required: false
 
-    type [Types::TransactionType], null: false
+    type [Types::ExpenseType], null: false
 
     def resolve(month: nil, category: nil)
       ## defaults as nil for both argument parameters and determines the proper response
       if month.present? && category.present?
-        object.transactions.where(month: month, category: category)
+        object.expenses.where(month: month, category: category)
       else
-        object.transactions
+        object.expenses
       end
     end
   end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,7 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :transactions, resolver: Resolvers::TransactionsResolver
+    field :expenses, resolver: Resolvers::ExpensesResolver
     field :budgets, resolver: Resolvers::BudgetsResolver
     field :username, String, null: false
     field :email, String, null: false
