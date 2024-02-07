d4bee20236459c217aa549af4383dfdc33c4c81a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-06 21:33:09 -0700

    Change dates on income and expense factories to be more dynamic

--- spec/factories/expense.rb
+++ spec/factories/expense.rb
@@ -7,6 +7,6 @@ FactoryBot.define do
     vendor { Faker::Company.name }
     category { Faker::Company.type }
     amount { Faker::Commerce.price(range: 0..999) }
-    date { Date.today }
+    date { Faker::Date.between(from: 100.days.ago, to: Date.today) }
   end
 end
