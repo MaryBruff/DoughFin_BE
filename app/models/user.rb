aa4470d95a3d6d762dac475b764093a63b00d281
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-06 19:36:32 -0700

    Write sql query to find monthly expenses and incomes per month and year

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,37 @@ class User < ApplicationRecord
                                            'expense' AS type FROM expenses WHERE user_id = #{id}
                                     ORDER BY date DESC")
   end
+
+  # select all incomes, group by month, alias of year, SUM of amount each month
+  def years_available # provides a unique list of years available for a user's incomes
+    incomes.map do |income|
+      income.year
+    end.uniq
+  end
+
+  def transaction_amounts_by_month_and_year
+    User.find_by_sql("SELECT 
+                        EXTRACT(YEAR FROM date) AS year,
+                        TO_CHAR(date, 'Month') AS month,
+                        SUM(amount) AS total_amount,
+                        'monthly_income' AS type 
+                      FROM incomes
+                      WHERE user_id = #{id}
+                      GROUP BY year, month
+                      UNION
+                      SELECT 
+                        EXTRACT(YEAR FROM date) AS year,
+                        TO_CHAR(date, 'Month') AS month,
+                        SUM(amount) AS total_amount,
+                        'monthly_expense' AS type 
+                      FROM expenses
+                      WHERE user_id = #{id}
+                      GROUP BY year, month
+                      ORDER BY year, month;")
+    # select amount and date from income and expenses
+    # pull month and year from date and print month as a word
+    # create monthly_income and monthly_expenses types column
+    # sum amounts on each table
+    # union between expenses and incomes where user_id = id
+  end
 end
