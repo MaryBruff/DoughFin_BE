878e02053e00b2c8a0dfd0b616addda06050901d
Anthea Yur <acyur6@gmail.com>
2024-02-08 01:10:17 -0600

    Structure the basics of calculating for a resolver

--- app/graphql/resolvers/budgets_resolver.rb
+++ app/graphql/resolvers/budgets_resolver.rb
@@ -9,9 +9,31 @@ module Resolvers
     def resolve(month: nil, category: nil)
       ## defaults as nil for both argument parameters and determines the proper response
       if month.present? && category.present?
-        object.budgets.where(month: month, category: category)
+        budgets = object.budgets.where(month: month, category: category)
+        expenses = Expense.all.where("to_char(date, 'YYYY-MM') = ? AND category = ?", month, category)
+        binding.pry
+        budget = calculate_expenses(budgets, expenses)
       else
         object.budgets
+      end
+    end
+
+    def calculate_expenses(budgets, expenses)
+      budgets.map do |budget|
+        sum_expenses = expenses.sum(:amount)
+        amount_remaining = budget.amount - sum_expenses
+        pct_remaining = if budget.amount > 0
+                         ((amnt_remaining/budget.amount) * 100).round(1)
+                        else
+                          0
+                        end
+
+        {
+          budget: budget,
+          binding.pry
+          amountRemaining: amount_remaining,
+          pctRemaining: pct_remaining
+        }
       end
     end
   end
