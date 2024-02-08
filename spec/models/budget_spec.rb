2e27cfe42c157fd09a9353e2636f3014bcab16d5
Anthea Yur <acyur6@gmail.com>
2024-02-08 02:00:39 -0600

    Add test coverage for budget methods for calculations

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Budget, type: :model do
   it { should validate_presence_of(:month) }
 
   describe "instance methods" do
-    describe "#categroies" do
+    describe "#categories" do
       it "returns all unique categories" do
         user = create :user
         5.times do
@@ -18,6 +18,32 @@ RSpec.describe Budget, type: :model do
 
         expect(user.budgets.categories.size).to eq 1
       end
+    end
+  end
+
+  describe '#pct_remaining, #amount_remaining' do
+    before :each do
+      @user = create :user
+      @budget = create(:budget, category: "Groceries", amount: 350.00, month: "2024-02")
+      @user.expenses = create_list(:expense, 5)
+      5.times do
+       @user.expenses << FactoryBot.create(:expense, amount: 25.00, user: @user, category: "Groceries", date: "2024-02-" + format('%02d', rand(1..28)))
+      end
+    end
+
+    it 'check instantiated data' do
+      budget_expenses = Expense.where(category: @budget.category)
+      expect(@user.expenses.length).to eq(10)
+      expect(budget_expenses.length).to eq(5)
+      expect(budget_expenses.sum(:amount)).to eq(125.0)
+    end
+
+    it '#pct_remaining calculates the percent remaining of that budget' do
+      expect(@budget.pct_remaining).to eq(64.3)
+    end
+
+    it '#amount_remaining calculates the amount remaining of that budget' do
+      expect(@budget.amount_remaining).to eq(225.0)
     end
   end
 end
