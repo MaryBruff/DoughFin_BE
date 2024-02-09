5ea0ca34223871562923b1a6ae8b67847d58d1ca
Taylor Pubins <tpubz@icloud.com>
2024-02-09 10:57:03 -0700

    Update create income/expenses specs

--- spec/graphql/mutations/incomes/create_income_request_spec.rb
+++ spec/graphql/mutations/incomes/create_income_request_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe Mutations::CreateIncome, type: :request do
       expect(income).to have_key(:date)
 
       expect(data[:userId]).to eq(user.id.to_s)
+      data = data[:income]
       expect(data[:source]).to eq("paycheck")
       expect(data[:amount]).to eq(2312.13)
       expect(data[:date]).to eq("2023-12-15")
